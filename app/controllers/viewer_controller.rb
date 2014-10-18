1dcc6e353516c38228235256363b542309ebc03b
Anthony Liekens <anthony@liekens.net>
2014-10-18 16:00:08 +0200

    check whether project exists in project viewer

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,6 +1,16 @@
 class ViewerController < ApplicationController
-  def showproject
+  
+  before_action :project_exists?
+  
+  def project_exists?
     @username = params[ "username" ]
     @name = params[ "name" ]
+    
+    if !Project.exists?( { username: @username, name: @name } )
+      redirect_to '/about/welcome', :flash => { :error => "We have not indexed this repository yet, sorry!" }
+    end
+  end
+  
+  def showproject
   end
 end
