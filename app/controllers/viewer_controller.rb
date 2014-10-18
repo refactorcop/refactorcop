825a1fd57157b5f9b29bbe43643ffe9b68783f7d
Maarten Claes <maartencls@gmail.com>
2014-10-19 00:38:43 +0200

    Fix N+1

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,21 +1,21 @@
 class ViewerController < ApplicationController
-  
+
   before_action :project_exists?
-  
+
   def project_exists?
     @username = params[ "username" ]
     @name = params[ "name" ]
-    
+
     if !Project.exists?( { username: @username, name: @name } )
       redirect_to '/about/welcome', :flash => { :error => "We have not indexed this repository yet, sorry!" }
     end
-    
+
     @project = Project.where( { username: @username, name: @name } ).first
-    
+
   end
-  
+
   def showproject
-    
+
     @offenses = @project.rubocop_offenses.order(
     "case rubocop_offenses.severity
       when 'convention' then 5
@@ -24,7 +24,7 @@ class ViewerController < ApplicationController
       when 'error' then 2
       when 'fatal' then 1
       else 99
-    end")
-    
+    end").includes(:source_file, :project)
+
   end
 end
