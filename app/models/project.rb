a317c34ec91af06ed59a745cfea55823501104eb
Dale Hofkens <dale@valid.be>
2014-10-18 12:00:04 +0200

    annotate models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  username    :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Project < ActiveRecord::Base
   validates :name, :username, presence: true, allow_blank: false
 
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  username    :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 FactoryGirl.define do
   factory :project do
     name "MyString"
