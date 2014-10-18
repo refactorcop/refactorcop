efda896cdedd15963c2fc6903f89b45ec7351f10
Maarten Claes <maartencls@gmail.com>
2014-10-18 19:48:18 +0200

    Create rubocop offense models when linting

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -6,7 +6,6 @@
 #  project_id       :integer
 #  content          :text
 #  path             :string(255)
-#  rubocop_offenses :json
 #  created_at       :datetime
 #  updated_at       :datetime
 #
@@ -21,10 +20,23 @@ class SourceFile < ActiveRecord::Base
   end
 
   def update_offenses
+    rubocop_path = Rails.root.join(".rubocop.yml")
     Tempfile.open(['offenses', '.rb']) do |f|
       f.write(content.encode('utf-8'))
-      json_data = `bundle exec rubocop --config ".rubocop.yml" -f json "#{f.path}"`
-      self.rubocop_offenses = json_data
+      f.read
+
+      json_data = `bundle exec rubocop --config "#{rubocop_path}" -f json "#{f.path}"`
+      json = MultiJson.load(json_data, symbolize_keys: true)
+      json[:files].first[:offenses].each do |offense_json|
+        self.rubocop_offenses.create({
+          severity:   offense_json[:severity],
+          message:    offense_json[:message],
+          cop_name:   offense_json[:cop_name],
+          location_column:  offense_json[:location][:column],
+          location_line:    offense_json[:location][:line],
+          location_length:  offense_json[:location][:length],
+        })
+      end
     end
     save!
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018170109) do
+ActiveRecord::Schema.define(version: 20141018172516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20141018170109) do
     t.integer  "project_id"
     t.text     "content"
     t.string   "path"
-    t.json     "rubocop_offenses"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
