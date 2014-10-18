a394b040860348d1af73128f1ecfbeaa03fa4793
Dale Hofkens <dale@valid.be>
2014-10-18 19:42:39 +0200

    add rollbar and OMG tablefunc

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20141018170109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-  enable_extension "tablefunc"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
