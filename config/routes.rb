1e919814c2ab7c4fc685ea620960b404842e64cf
Anthony Liekens <anthony@liekens.net>
2014-10-19 01:18:22 +0200

    dashboards skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/scheduler'
   
   get ':username/:name', controller: "viewer", action: "showproject"
+  get ':username/:name/dashboards', controller: "viewer", action: "showdashboards"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
