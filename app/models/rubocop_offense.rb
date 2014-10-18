f5c66a116260c2917a1c9fd8e7a53958dd3d639a
Dale Hofkens <dale@valid.be>
2014-10-19 00:38:28 +0200

    improve showproject performance

--- app/models/rubocop_offense.rb
+++ app/models/rubocop_offense.rb
@@ -17,18 +17,19 @@
 class RubocopOffense < ActiveRecord::Base
   validates_presence_of :message, :cop_name, :severity
   belongs_to :source_file
+  has_one :project, through: :source_file
 
   def line_range
     (location_line-1)..(location_line + location_length)
   end
-  
+
   def to_html
     lines = self.source_file.content.lines[self.line_range]
     CodeRay.scan(lines.join, :ruby).div({
       line_numbers: :inline,
       line_number_start: self.location_line - 1,
       highlight_lines: self.location_line,
-      line_number_anchors: false 
+      line_number_anchors: false
     })
   end
 
