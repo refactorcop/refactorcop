155eeb33870cd1df5196722a3f3f3ed7ec4edd36
Dale Hofkens <dale@valid.be>
2014-10-18 18:43:00 +0200

    improve dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,29 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+       column do
+         panel "Recent Projects" do
+           ul do
+             Project.order("created_at DESC").take(5).map do |project|
+               li link_to(project.full_name, admin_project_path(project))
+             end
+           end
+         end
+       end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+       column do
+         panel "Worst Projects" do
+           ul do
+             Project.order("rubocop_offenses_count DESC").take(5).map do |project|
+               li link_to(project.full_name, admin_project_path(project))
+             end
+           end
+         end
+       end
+     end
   end # content
 end
