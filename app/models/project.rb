ed470a28471e6637b48cf20958912707da52cf58
Maarten Claes <maartencls@gmail.com>
2014-10-18 11:22:08 +0200

    Remove github_path

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,6 @@
 class Project < ActiveRecord::Base
   validates :name, :username, presence: true, allow_blank: false
 
-  def github_path
-    "/#{username}/#{name}"
-  end
-
   def clone_url
     "git@github.com:#{username}/#{name}.git"
   end
