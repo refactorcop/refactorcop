23a2e5be5f45a094c1c1b39d1d5cd1cc21431b36
Dale Hofkens <dale@valid.be>
2014-10-18 18:28:55 +0200

    annotate

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -2,13 +2,15 @@
 #
 # Table name: projects
 #
-#  id              :integer          not null, primary key
-#  name            :string(255)
-#  username        :string(255)
-#  description     :text
-#  created_at      :datetime
-#  updated_at      :datetime
-#  repository_data :json
+#  id                     :integer          not null, primary key
+#  name                   :string(255)
+#  username               :string(255)
+#  description            :text
+#  created_at             :datetime
+#  updated_at             :datetime
+#  repository_data        :json
+#  source_files_count     :integer          default(0), not null
+#  rubocop_offenses_count :integer          default(0), not null
 #
 
 FactoryGirl.define do
