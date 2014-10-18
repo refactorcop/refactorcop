407b3b82c46e9fb3c76ea304c928224e5ab4662a
Dale Hofkens <dale@valid.be>
2014-10-19 01:44:08 +0200

    fix tablename

--- db/migrate/20141018231244_add_rubocop_run_timestamp_to_project.rb
+++ db/migrate/20141018231244_add_rubocop_run_timestamp_to_project.rb
@@ -1,6 +1,6 @@
 class AddRubocopRunTimestampToProject < ActiveRecord::Migration
   def change
-    add_column :project, :rubocop_run_started_at, :integer, default: 0, null: false
-    add_column :project, :rubocop_run_dispatched_at, :integer, default: 0, null: false
+    add_column :projects, :rubocop_run_started_at, :integer, default: 0, null: false
+    add_column :projects, :rubocop_run_dispatched_at, :integer, default: 0, null: false
   end
 end
