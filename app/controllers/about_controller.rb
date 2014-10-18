e9377fa976a0c992cda012afa66bdebed1154d51
Anthony Liekens <anthony@liekens.net>
2014-10-18 18:01:18 +0200

    basic search functionality

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,5 +2,12 @@ class AboutController < ApplicationController
   def welcome
     # pick a lucky winner for the i'm feeling lucky button
     @random_project = Project.limit(1).offset(rand(Project.count)).first
+    if params[ "query" ]
+      @query = params[ "query" ]
+      @projects = Project.where( "name ILIKE ? or description ILIKE ?", "%#{@query}%",  "%#{@query}%" )
+    else
+      @query = nil
+      @projects = Project.all
+    end
   end
 end
