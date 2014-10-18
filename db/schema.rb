e93d0d63818823937cbac913109eea0a8fd7e81d
Dale Hofkens <dale@valid.be>
2014-10-18 12:03:02 +0200

    remove tablefunc and hstore from our schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 20141018091121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-  enable_extension "tablefunc"
 
   create_table "projects", force: true do |t|
     t.string   "name"
