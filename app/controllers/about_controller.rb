2e81445e754539769e6aa3a951988d767b50bfef
Anthony Liekens <anthony@liekens.net>
2014-10-18 16:46:41 +0200

    wip list of projects and project page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,6 @@
 class AboutController < ApplicationController
   def welcome
+    # pick a lucky winner for the i'm feeling lucky button
+    @random_project = Project.limit(1).offset(rand(Project.count)).first
   end
 end
