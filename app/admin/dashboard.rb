1828c4914cceb8fc9ca63755f793b8754f8801cc
Dale Hofkens <dale@valid.be>
2014-10-19 13:18:55 +0200

    rubocop formatting

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -7,25 +7,25 @@ ActiveAdmin.register_page "Dashboard" do
     # Here is an example of a simple dashboard with columns and panels.
     #
     columns do
-       column do
-         panel "Recent Projects" do
-           ul do
-             Project.order("created_at DESC").take(5).map do |project|
-               li link_to(project.full_name, admin_project_path(project))
-             end
-           end
-         end
-       end
+      column do
+        panel "Recent Projects" do
+          ul do
+            Project.order("created_at DESC").take(5).map do |project|
+              li link_to(project.full_name, admin_project_path(project))
+            end
+          end
+        end
+      end
 
-       column do
-         panel "Worst Projects" do
-           ul do
-             Project.order("rubocop_offenses_count DESC").take(5).map do |project|
-               li link_to(project.full_name, admin_project_path(project))
-             end
-           end
-         end
-       end
-     end
+      column do
+        panel "Worst Projects" do
+          ul do
+            Project.order("rubocop_offenses_count DESC").take(5).map do |project|
+              li link_to(project.full_name, admin_project_path(project))
+            end
+          end
+        end
+      end
+    end
   end # content
 end
