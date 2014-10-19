7035a650f67d4b475ba16b74faca93cdcbdb4dc4
Maarten Claes <maartencls@gmail.com>
2014-10-19 23:26:39 +0200

    Redirect for screenshot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :check_screenshot
+
   rescue_from 'ActionController::RoutingError' do |e|
     redirect_to action: :project_not_found, controller: :viewer
   end
 
+
   def authenticate_admin
     authenticate_or_request_with_http_basic("Recovering Vegetarian Area") do |name, password|
       name == "admin" && password == "falafel"
+    end
+  end
+
+  protected
+
+  def check_screenshot
+    unless params[:screenshot].blank?
+      redirect_to "/screenshot.html"
     end
   end
 end
