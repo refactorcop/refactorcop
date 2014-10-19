c13bc639ceb80c6ee807c6ea3f12e07ad13956fd
Maarten Claes <maartencls@gmail.com>
2014-10-19 13:19:15 +0200

    View object for project details added

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,36 +1,11 @@
 class ViewerController < ApplicationController
-  before_action :project_exists?, only: [:showproject]
-
-  def project_exists?
-    @username = params["username"]
-    @name = params["name"]
-
-    @project = Project.where(username: @username, name: @name).first
-    unless @project
-      redirect_to '/about/welcome', :flash => { :error => "We have not indexed this repository yet, sorry!" }
-    end
-  end
-
   def showproject
-    # dashboards
-    all_offenses = RubocopOffense.includes(:source_file).where(source_files: { project_id: @project })
-    @total_count    = all_offenses.count
-    @severity_count = all_offenses.group(:severity).count.sort_by{|k,v| v}.reverse
-    @copname_count  = all_offenses.group(:cop_name).count.sort_by{|k,v| v}.reverse
-
-    # paginated view
-
-    @offenses = @project.rubocop_offenses.order(
-    "case rubocop_offenses.severity
-      when 'convention' then 5
-      when 'warning' then 4
-      when 'refactor' then 3
-      when 'error' then 2
-      when 'fatal' then 1
-      else 99
-    end").includes(:source_file, :project)
-
-    @offenses = @offenses.page(params[:page]).per(10)
+    @project_details = ProjectDetails.new(params)
+    unless @project_details.exists?
+      redirect_to '/about/welcome', :flash => {
+        :error => "We have not indexed this repository yet, sorry!"
+      }
+    end
   end
 
   def random
---
+++ app/view_objects/project_details.rb
@@ -0,0 +1,56 @@
+class ProjectDetails
+  attr_reader :params
+
+  def initialize(params)
+    @params = params
+  end
+
+  def project
+    @project ||= Project.where(username: username, name: name).first
+  end
+
+  def exists?
+    !project.blank?
+  end
+
+  def username
+    @username ||= params[:username].freeze
+  end
+
+  def name
+    @name ||= params[:name].freeze
+  end
+
+  def offenses
+    @offenses ||= project.rubocop_offenses
+      .includes(:source_file, :project)
+      .order("""
+    case rubocop_offenses.severity
+      when 'convention' then 5
+      when 'warning' then 4
+      when 'refactor' then 3
+      when 'error' then 2
+      when 'fatal' then 1
+      else 99
+    end""")
+      .page(params[:page]).per(10)
+  end
+
+  def total_offense_count
+    @total_count    ||= all_offenses.count
+  end
+
+  def offense_count_per_severity
+    @per_severity_count ||= all_offenses.group(:severity).count.sort_by{|k,v| v}.reverse
+  end
+
+  def offense_count_per_cop_name
+    @per_copname_count ||= all_offenses.group(:cop_name).count.sort_by{|k,v| v}.reverse
+  end
+
+  private
+
+  def all_offenses
+    @all_offenses ||= RubocopOffense.includes(:source_file).where(source_files: { project_id: project.id })
+  end
+end
