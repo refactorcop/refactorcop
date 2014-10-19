0540c322d026e6167f18c1bc4b8e018c3b118f6d
Maarten Claes <maartencls@gmail.com>
2014-10-19 13:03:22 +0200

    Added nprogress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//
+//= require nprogress
+//= require nprogress-turbolinks
+//
 //= require_tree .
