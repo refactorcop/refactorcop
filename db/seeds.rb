e5242dbe0317d558fbf6b367bf789dbb46a8634f
Maarten Claes <maartencls@gmail.com>
2014-10-27 20:33:52 +0100

    Added seed script for `rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Importing trending Ruby projects from Github:"
+projects = GithubTrending.new('ruby').persist_projects
+projects.each do |project|
+  puts "* Linting #{project.full_name.inspect}"
+  RubocopWorker.new.perform(project.id)
+end
+puts "Done."
