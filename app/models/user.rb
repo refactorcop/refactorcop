d01f8c7a4ebd56171940bb702ef9e2f56dea130b
Raphael Costa <raphael@raphaelcosta.net>
2015-01-09 19:31:12 -0200

    users area on admin interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :projects, foreign_key: 'owner_id'
+
   validates_presence_of :email, :github_uid, :github_token
 
   def self.find_or_create_from_github_auth(auth_hash)
@@ -6,6 +8,10 @@ class User < ActiveRecord::Base
       u.github_token = auth_hash[:credentials][:token]
       u.email = auth_hash[:info][:email]
     end
+  end
+
+  def projects_count
+    projects.size
   end
 
   def github_client
