1cc0bcc5c98734e7582b72913a33b9a1fba2d234
Maarten Claes <maartencls@gmail.com>
2015-04-25 16:12:51 +0200

    Extract logic related zip file

--- app/models/project/download.rb
+++ app/models/project/download.rb
@@ -15,7 +15,9 @@ class Project::Download
   def call
     project.source_files.delete_all
     Tempfile.create([filename, ".zip"], :encoding => 'ascii-8bit') do |zip_file|
-      http_client.get_content(project.download_zip_url) { |chunk| zip_file.write(chunk) }
+      http_client.get_content(project.download_zip_url) { |chunk|
+        zip_file.write(chunk)
+      }
       rescue_reopen_error { unzip_to_source_files(zip_file) }
     end
   end
@@ -42,38 +44,8 @@ class Project::Download
 
   # @param file [String]
   def unzip_to_source_files(file)
-    Zip::File.open_buffer(file) do |zip_file|
-      @project.has_todo = zip_file.glob('**/.rubocop_todo.yml').present?
-      zip_file.glob('**/*.rb').compact.map do |entry|
-        next if ignore_file?(entry.name)
-        to_source_file(entry)
-      end
-    end
-  end
-
-  def ignore_file?(filename)
-    SourceFile::IgnoreCheck.call(filename)
-  end
-
-  def to_source_file(entry)
-    sf = SourceFile.new(project: project, path: sanitize_file_path(entry))
-    input_stream = entry.get_input_stream
-    sf.content = input_stream.read if input_stream.respond_to?(:read)
-    sf.save!
-    sf
-  rescue ActiveRecord::StatementInvalid => e
-    logger.warn { "While inserting SourceFile content, ignoring exception: #{e.inspect}" }
-    sf.update_attribute(:content, nil)
-    sf
-  rescue StandardError => e
-    logger.warn "FAILED ON FILE: #{entry.name} #{e.inspect}"
-    nil
-  end
-
-  # Pops of the name of the project directory ( == name of zip file )
-  def sanitize_file_path(entry)
-    parts = entry.name.split(File::SEPARATOR)
-    parts.shift
-    parts.join(File::SEPARATOR)
+    zipfile = Project::ZipFile.new(file, project: project)
+    project.has_todo = zipfile.has_rubocop_todos?
+    zipfile.source_files
   end
 end
