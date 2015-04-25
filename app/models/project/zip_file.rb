0b0e39c29b340776042d47fb3160a32441ac70b3
Maarten Claes <maartencls@gmail.com>
2015-04-25 17:26:46 +0200

    Test Project::ZipFile logic

--- app/models/project/zip_file.rb
+++ app/models/project/zip_file.rb
@@ -1,7 +1,7 @@
 class Project::ZipFile
   attr_reader :filepath
 
-  def initialize(filepath)
+  def initialize(filepath, in_memory: true)
     @filepath = filepath
     @parsed = false
   end
@@ -23,10 +23,18 @@ class Project::ZipFile
 
   def parse_zipfile
     return if @parsed
-    Zip::File.open_buffer(filepath) do |zf|
+    open_zip_file(filepath) do |zf|
       @has_rubocop_todos = zf.glob('**/.rubocop_todo.yml').present?
       @ruby_entries = zf.glob('**/*.rb').compact
     end
     @parsed = true
+  end
+
+  def open_zip_file(filepath, &block)
+    if @in_memory
+      Zip::File.open_buffer(filepath, &block)
+    else
+      Zip::File.open(filepath, &block)
+    end
   end
 end
