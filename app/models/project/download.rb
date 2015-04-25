48e9c31da13dfb63581a0c90572c97501f0cd27e
Maarten Claes <maartencls@gmail.com>
2015-04-25 16:58:14 +0200

    Don't have Project::ZipFile depend on Project or SourceFile

--- app/models/project/download.rb
+++ app/models/project/download.rb
@@ -1,6 +1,6 @@
 require 'zip'
 
-# Download a project's code from GitHub
+# Download a project's code from GitHub. Stores it in {SourceFile} models.
 class Project::Download
   include Procto.call
 
@@ -18,7 +18,7 @@ class Project::Download
       http_client.get_content(project.download_zip_url) { |chunk|
         zip_file.write(chunk)
       }
-      unzip_to_source_files(zip_file)
+      unzip_and_update_project(zip_file)
     end
   end
 
@@ -29,9 +29,48 @@ class Project::Download
   end
 
   # @param file [String]
-  def unzip_to_source_files(file)
-    zipfile = Project::ZipFile.new(file, project: project)
+  def unzip_and_update_project(filepath)
+    zipfile = Project::ZipFile.new(filepath)
     project.has_todo = zipfile.has_rubocop_todos?
-    zipfile.source_files
+    save_as_source_files(zipfile.ruby_entries)
+  end
+
+  # Saves the files as {SourceFile} models
+  #
+  # @param [Array<Zip::Entry>] entries
+  #
+  # @return [undefined]
+  def save_as_source_files(entries)
+    entries.each do |entry|
+      next if SourceFile::IgnoreCheck.call(entry.name)
+      save_as_source_file(entry)
+    end
+  end
+
+  def save_as_source_file(entry)
+    sf = SourceFile.new(
+      project:  project,
+      path:     sanitize_file_path(entry),
+      content:  read_content(entry),
+    )
+    return if sf.content.blank?
+    sf.save!
+    sf
+  end
+
+  # @param [Zip::Entry] entry
+  def read_content(entry)
+    input = entry.get_input_stream
+    input.read if input.respond_to?(:read)
+  rescue StandardError => e
+    logger.warn "FAILED ON FILE: #{entry.name} #{e.inspect}"
+    nil
+  end
+
+  # Pops of the name of the project directory ( == name of zip file )
+  def sanitize_file_path(entry)
+    parts = entry.name.split(File::SEPARATOR)
+    parts.shift
+    parts.join(File::SEPARATOR)
   end
 end
--- app/models/project/zip_file.rb
+++ app/models/project/zip_file.rb
@@ -1,64 +1,32 @@
 class Project::ZipFile
-  attr_reader :filepath, :project
+  attr_reader :filepath
 
-  def initialize(filepath, project:, logger: Rails.logger)
+  def initialize(filepath)
     @filepath = filepath
-    @project = project
     @parsed = false
-    @logger = logger
-  end
-
-  def source_files
-    parse_file
-    @source_files
   end
 
   def has_rubocop_todos?
-    parse_file
+    parse_zipfile
     !!@has_rubocop_todos
+  end
+
+  # Returns all files with a `.rb` extension
+  #
+  # @return [Array<Zip::Entry>]
+  def ruby_entries
+    parse_zipfile
+    @ruby_entries
   end
 
   private
 
-  attr_reader :logger
-
-  def parse_file
+  def parse_zipfile
     return if @parsed
     Zip::File.open_buffer(filepath) do |zf|
       @has_rubocop_todos = zf.glob('**/.rubocop_todo.yml').present?
-      @source_files = zf.glob('**/*.rb').compact.map do |entry|
-        next if ignore_file?(entry.name)
-        to_source_file(entry)
-      end
+      @ruby_entries = zf.glob('**/*.rb').compact
     end
     @parsed = true
-  end
-
-  def ignore_file?(filename)
-    SourceFile::IgnoreCheck.call(filename)
-  end
-
-  def to_source_file(entry)
-    sf = SourceFile.new(project: project, path: sanitize_file_path(entry))
-    input_stream = entry.get_input_stream
-    sf.content = input_stream.read if input_stream.respond_to?(:read)
-    sf.save!
-    sf
-  rescue ActiveRecord::StatementInvalid => e
-    logger.warn {
-      "While inserting SourceFile content, ignoring exception: #{e.inspect}"
-    }
-    sf.update_attribute(:content, nil)
-    sf
-  rescue StandardError => e
-    logger.warn "FAILED ON FILE: #{entry.name} #{e.inspect}"
-    nil
-  end
-
-  # Pops of the name of the project directory ( == name of zip file )
-  def sanitize_file_path(entry)
-    parts = entry.name.split(File::SEPARATOR)
-    parts.shift
-    parts.join(File::SEPARATOR)
   end
 end
