7b551e8bd8d0dd81544b56cf824dd59e012bd028
Maarten Claes <maartencls@gmail.com>
2015-10-11 11:44:56 +0200

    Fix selector in specs

--- spec/features/checking_repositories_spec.rb
+++ spec/features/checking_repositories_spec.rb
@@ -11,13 +11,13 @@ RSpec.feature "Checking repositories", :type => :feature do
   scenario "Visiting an indexed repository" do
     create(:project, username: "Homebrew", name: "homebrew")
     visit "/homebrew/homebrew"
-    expect(page.find(".project_details")).to have_content("homebrew / homebrew")
+    expect(page.find(".project-details")).to have_content("homebrew / homebrew")
   end
 
   scenario "Visiting a repository with dots in its name" do
     create(:project, username: "github", name: "developer.github.com")
     visit "/github/developer.github.com"
-    expect(page.find(".project_details")).to have_content("github / developer.github.com")
+    expect(page.find(".project-details")).to have_content("github / developer.github.com")
   end
 
   scenario "Visiting a project that hasn't been added yet" do
