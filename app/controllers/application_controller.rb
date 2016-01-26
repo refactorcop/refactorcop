c967e6699da18a258c4fdf65050e9d620f2fc585
Maarten Claes <maartencls@gmail.com>
2016-01-26 22:13:36 +0100

    Remove screenshot redirect
    This was only used by the RailsRumble bot to get a proper screenshot to
    use on the project pages. The competition is over so this is no longer
    needed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :check_screenshot
-
   def authenticate_admin
     authenticate_or_request_with_http_basic("Recovering Vegetarian Area") do |username, password|
       AdminAuthentication.call(username, password)
     end
-  end
-
-  protected
-
-  def check_screenshot
-    redirect_to "/screenshot.html" unless params[:screenshot].blank?
   end
 end
